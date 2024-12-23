da0d8146a29e72819ac7c7e8a3546ec53068d216
congfu2310 <hm2426@wayne.edu>
2024-12-23 11:57:57 -0500

    new

--- model/src/main/java/com/atguigu/ssyx/vo/product/AttrGroupQueryVo.java
+++ model/src/main/java/com/atguigu/ssyx/vo/product/AttrGroupQueryVo.java
@@ -5,9 +5,9 @@ import java.util.Date;
 import io.swagger.annotations.ApiModelProperty;
 
 @Data
-public class CategoryQueryVo {
+public class AttrGroupQueryVo {
 	
-	@ApiModelProperty(value = "分类名称")
+	@ApiModelProperty(value = "组名")
 	private String name;
 
 }
